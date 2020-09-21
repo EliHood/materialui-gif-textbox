598f856e2d8994928f71f7b8048a10a961d5ed96
elihood <eli@elihoods-MacBook-Pro.local>
2020-09-21 12:21:03 -0400

    update

--- src/components/CommentBox.test.tsx
+++ src/components/CommentBox.test.tsx
@@ -69,6 +69,12 @@ describe("CommentBox Test", () => {
     expect(screen.getByTestId("comment-button")).toBeDisabled();
   });
 
+  it("should test onChange TextField", () => {
+    const field = screen.getByTestId("comment_input").querySelector("textarea");
+    fireEvent.change(field, { target: { defaultValue: "Owls Are Cool" } });
+    expect(field.defaultValue).toBe("Owls Are Cool");
+  });
+
   it("should click gif icon and click text icon ", async () => {
     fireEvent.click(screen.getByTestId("gif-icon"));
     await waitFor(() =>
